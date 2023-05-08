604f9e90f30eb8b0cb77cba5748c7f5fefe2018c
guasonsad <nikole2019@hotmail.com>
2023-05-07 22:40:30 -0500

    Create Providers

---
+++ Pages/Providers/Create.cshtml.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using SupermarketWEB.Data;
+using SupermarketWEB.Models;
+using System.Threading.Tasks;
+
+namespace SupermarketWEB.Pages.Providers
+{
+	public class CreateModel : PageModel
+	{
+		private readonly SupermarketContext _context;
+
+		public CreateModel(SupermarketContext context)
+		{
+			_context = context;
+		}
+
+		public IActionResult OnGet()
+		{
+			return Page();
+		}
+
+		[BindProperty]
+		public Provider Provider { get; set; }
+
+		public async Task<IActionResult> OnPostAsync()
+		{
+			if (!ModelState.IsValid)
+			{
+				return Page();
+			}
+
+			_context.Providers.Add(Provider);
+			await _context.SaveChangesAsync();
+
+			return RedirectToPage("./Index");
+		}
+	}
+}
