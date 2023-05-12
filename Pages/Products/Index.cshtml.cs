861828be8b0a65b4b7656a3e36999f4b41bfe247
guasonsad <nikole2019@hotmail.com>
2023-05-11 23:28:32 -0500

    Index Products

---
+++ Pages/Products/Index.cshtml.cs
@@ -0,0 +1,45 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SupermarketWEB.Data;
+using SupermarketWEB.Models;
+
+namespace SupermarketWEB.Pages.Products
+{
+	public class IndexModel : PageModel
+	{
+		private readonly SupermarketContext _context;
+
+		public IndexModel(SupermarketContext context)
+		{
+			_context = context;
+		}
+
+		public IList<Product> Products { get; set; }
+
+		public async Task<IActionResult> OnGetAsync()
+		{
+			Products = await _context.Products
+				.Include(p => p.Category)
+				.ToListAsync();
+			return Page();
+		}
+
+		public async Task<IActionResult> OnPostDeleteAsync(int id)
+		{
+			var product = await _context.Products.FindAsync(id);
+
+			if (product == null)
+			{
+				return NotFound();
+			}
+
+			_context.Products.Remove(product);
+			await _context.SaveChangesAsync();
+
+			return RedirectToPage("./Index");
+		}
+	}
+}
