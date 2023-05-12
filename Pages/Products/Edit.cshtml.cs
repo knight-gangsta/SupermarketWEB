50889c6ca99ca516118808fc73fa84024af50ed0
guasonsad <nikole2019@hotmail.com>
2023-05-12 00:22:04 -0500

    Edit Product

---
+++ Pages/Products/Edit.cshtml.cs
@@ -0,0 +1,82 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SupermarketWEB.Data;
+using SupermarketWEB.Models;
+
+namespace SupermarketWEB.Pages.Products
+{
+	public class EditModel : PageModel
+	{
+		private readonly SupermarketContext _context;
+
+		public EditModel(SupermarketContext context)
+		{
+			_context = context;
+		}
+
+		[BindProperty]
+		public Product Product { get; set; }
+
+		public SelectList CategoryList { get; set; }
+
+		public async Task<IActionResult> OnGetAsync(int? id)
+		{
+			if (id == null)
+			{
+				return NotFound();
+			}
+
+			Product = await _context.Products
+				.Include(p => p.Category)
+				.FirstOrDefaultAsync(m => m.Id == id);
+
+			if (Product == null)
+			{
+				return NotFound();
+			}
+
+			// Set the category list for the dropdown
+			CategoryList = new SelectList(_context.Categories, "Id", "Name", Product.CategoryId);
+
+			return Page();
+		}
+
+		public async Task<IActionResult> OnPostAsync()
+		{
+			if (!ModelState.IsValid)
+			{
+				// Set the category list for the dropdown
+				CategoryList = new SelectList(_context.Categories, "Id", "Name", Product.CategoryId);
+				//return Page();
+			}
+
+			_context.Attach(Product).State = EntityState.Modified;
+
+			try
+			{
+				await _context.SaveChangesAsync();
+			}
+			catch (DbUpdateConcurrencyException)
+			{
+				if (!ProductExists(Product.Id))
+				{
+					return NotFound();
+				}
+				else
+				{
+					throw;
+				}
+			}
+
+			return RedirectToPage("./Index");
+		}
+
+		private bool ProductExists(int id)
+		{
+			return _context.Products.Any(e => e.Id == id);
+		}
+	}
+}
+
