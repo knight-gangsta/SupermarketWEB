3d6aaad328bb089c6fa4e3490c6f4f2516cc6635
guasonsad <nikole2019@hotmail.com>
2023-05-11 23:49:57 -0500

    Delete Product

--- Pages/Products/Create.cshtml.cs
+++ Pages/Products/Create.cshtml.cs
@@ -17,40 +17,40 @@ namespace SupermarketWEB.Pages.Products
 			_context = context;
 		}
 
+		
 		public List<Category> CategoryList { get; set; }
 
+		
 		public async Task<IActionResult> OnGetAsync()
 		{
+			
 			CategoryList = await _context.Categories.ToListAsync();
+
+			
 			return Page();
 		}
 
+		
 		[BindProperty]
 		public Product Product { get; set; }
 
+		
 		public async Task<IActionResult> OnPostAsync()
 		{
+			
+			Console.WriteLine(Product);
 			if (!ModelState.IsValid || Product == null || _context.Products == null)
 			{
+				
 				CategoryList = await _context.Categories.ToListAsync();
-				return Page();
+				
 			}
 
-			// Convertir el valor de Price a decimal
-			if (decimal.TryParse(Product.Price, out decimal price))
-			{
-				Product.Price = price;
-			}
-			else
-			{
-				ModelState.AddModelError("Product.Price", "El precio no es válido.");
-				CategoryList = await _context.Categories.ToListAsync();
-				return Page();
-			}
-
+			
 			_context.Products.Add(Product);
 			await _context.SaveChangesAsync();
 
+			
 			return RedirectToPage("./Index");
 		}
 	}
